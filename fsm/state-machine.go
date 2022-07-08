65187777190264c1e9ca112651d16403ff5e843f
cgxarrie <cgxarrie@users.noreply.github.com>
2022-07-08 15:05:57 +0200

    change Transitions to a map

--- fsm/state-machine.go
+++ fsm/state-machine.go
@@ -1,20 +1,18 @@
 package fsm
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type State uint32
 
 type Command uint32
 
-type Transition struct {
-	From    State
-	Command Command
-	To      State
-}
+type transition map[Command]map[State]State
 
 type StateMachine struct {
 	State       State
-	Transitions []Transition
+	Transitions transition
 }
 
 func New(initialState State) StateMachine {
@@ -26,38 +24,53 @@ func New(initialState State) StateMachine {
 
 func (fsm *StateMachine) AddTransition(from State, command Command, to State) bool {
 
-	_, err := fsm.findTransition(command)
-	if err == nil {
-		return false
+	if fsm.Transitions == nil {
+		fsm.Transitions = transition{
+			command: {
+				from: to,
+			},
+		}
+		return true
 	}
 
-	fsm.Transitions = append(fsm.Transitions, Transition{From: from, Command: command, To: to})
-	return true
+	_, commandExists := fsm.Transitions[command]
+
+	if !commandExists {
+		fsm.Transitions[command] = map[State]State{from: to}
+		return true
+	}
+
+	_, fromStateExists := fsm.Transitions[command][from]
+
+	if !fromStateExists {
+		fsm.Transitions[command][from] = to
+		return true
+	}
+
+	return false
 }
 
 func (fsm *StateMachine) ExecuteCommand(command Command) (bool, error) {
-	transition, err := fsm.findTransition(command)
 
-	if err != nil {
-		return false, err
+	canExecute := fsm.canExecuteCommand(command)
+
+	if !canExecute {
+		return false, fmt.Errorf("cannot execute requested command %v from state %v", command, fsm.State)
 	}
 
-	fsm.State = transition.To
+	fsm.State = fsm.Transitions[command][fsm.State]
 	return true, nil
 }
 
-func (fsm StateMachine) findTransition(command Command) (*Transition, error) {
+func (fsm StateMachine) canExecuteCommand(command Command) bool {
 
-	for _, transition := range fsm.Transitions {
+	commandStates, commandExist := fsm.Transitions[command]
 
-		if transition.From != fsm.State || transition.Command != command {
-			continue
-		}
-
-		if transition.From == fsm.State && transition.Command == command {
-			return &transition, nil
-		}
+	if !commandExist {
+		return false
 	}
 
-	return nil, fmt.Errorf("Transition not found : Command%d from status %d", command, fsm.State)
+	_, fromSatateExist := commandStates[fsm.State]
+
+	return fromSatateExist
 }
